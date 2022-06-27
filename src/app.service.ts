e55bbf4993c0b65acdc61254e83bff6caef2f7e6
Nguyen Hong Hai <nguyenhonghai@cryptopie-labo.com>
2022-06-27 17:25:28 +0700

    update crontime

--- src/app.service.ts
+++ src/app.service.ts
@@ -14,9 +14,9 @@ export class AppService {
     private httpService: HttpService,
   ) {}
 
-  @Cron('0 0 18 * * 1-5', {
+  @Cron('0 0 23 * * 1-5', {
     timeZone: 'Asia/Ho_Chi_Minh',
-  }) // 18h00 T2 -> t6
+  }) // 23h00 T2 -> t6
   public async reportTimeLog(): Promise<void> {
     if (
       !this.configService.get('token') ||
